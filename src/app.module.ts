11c58aa4f4287db37ab4811eb54cd939d0c721bc
Arijit NeoSOFT <arijit.das@neosoftmail.com>
2025-07-04 16:14:19 +0530

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from './config/config.module.js';
-import { AuthModule } from './module/auth/auth.module.js';
-import { UserModule } from './module/user/user.module.js';
+import { AuthModule } from './rest/auth/auth.module.js';
+import { UserModule } from './rest/user/user.module.js';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://root:password@localhost:27017/mydb?authSource=admin'), AuthModule, ConfigModule, UserModule],
